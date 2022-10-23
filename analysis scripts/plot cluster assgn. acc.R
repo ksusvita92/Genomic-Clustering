b2dbc826ddfdc1794e6695bb4a4f22f1f2bf68cc
Kurnia Susvitasari <ksusvita@sfu.ca>
2022-10-23 15:48:40 -0700

    Figure 4

--- analysis scripts/plot cluster assgn. acc.R
+++ analysis scripts/plot cluster assgn. acc.R
@@ -1,4 +1,4 @@
-# Title: Plot Figure 5
+# Title: Reproduce Figure 5 and Table 4
 # 
 # Description:
 #
@@ -11,14 +11,24 @@ library(ggplot2)
 library(dplyr)
 
 dt <- read.csv("./results/cluster_assgn_acc.csv")
+dt$method <- factor(dt$method, levels = c("plr", "mlr", "random"))
 
 
 
 ## ******* Create boxplot *******
 ## ******************************
 dt %>% ggplot() +
-  geom_boxplot(aes(x = K, y = acc, fill = method)) +
+  geom_boxplot(aes(x = K, y = acc, fill = method), alpha = .4) +
   facet_wrap(~model) +
   labs(x = "", y = "accuracy") +
-  theme_light()
+  theme_light() +
+  theme(legend.position = "top", legend.title = element_blank())
 #
+
+
+
+## ******* Table 4 *******
+## ***********************
+dt %>% group_by(model, method, K) %>% summarise(m.acc = mean(acc), sd.acc = sd(acc))
+
+
