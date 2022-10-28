f17ffa334c9c316ef4e28124c2c46def1d215421
Kurnia Susvitasari <ksusvita@sfu.ca>
2022-10-28 11:27:54 -0700

    Clean TB data in Valencia

--- analysis scripts/tb_valencia.R
+++ analysis scripts/tb_valencia.R
@@ -21,20 +21,18 @@ library(dplyr)
 url <- "https://elifesciences.org/download/aHR0cHM6Ly9jZG4uZWxpZmVzY2llbmNlcy5vcmcvYXJ0aWNsZXMvNzY2MDUvZWxpZmUtNzY2MDUtc3VwcDEtdjEueGxzeA--/elife-76605-supp1-v1.xlsx?_hash=pzQwKD1DzDLre7kKrWI%2Fhd%2BjY2FGgpekrPI4vXrlWNo%3D"
 destfile <- "rawdt.xlsx"
 curl::curl_download(url, destfile)
-rawdt <- read_excel(destfile, range = "A3:AB778")
+rawdt <- read_excel(destfile, range = "A3:AB778", na = "NA")
+
 
 loc <- read.csv("./analysis scripts/location_data.csv")
 
 # subset the necessary column and cases
-rawdt <- rawdt %>% transmute(ID = ...1, Cluster = `Genomic
+dt <- rawdt %>% transmute(ID = ...1, Cluster = `Genomic
 Cluster ID`, Gender = Gender, Foreign = ifelse(`Country of birth`=="SPAIN", "No", "YES"), Diabetes = Diabetes, HIV = `HIV infected`) %>%
   filter(!is.na(Gender), !is.na(Foreign), !is.na(Diabetes), !is.na(HIV)) %>%
-  inner_join(loc, by = c("ID"="ID_server"))
-#
+  inner_join(loc, by = c("ID"))
 
+nm <- as.data.frame(table(dt$Cluster)) %>% filter(Freq > 2) %>% pull(Var1)
+dt <- dt %>% filter(Cluster %in% nm)
 
-nm <- as.data.frame(table(rawdt$Cluster)) %>% filter(Freq > 2) %>% pull(Var1)
-dt <- rawdt %>% filter(Cluster %in% nm)
-
-
-rm(loc, destfile, url, nm)
+rm(loc, destfile, url, rawdt)
