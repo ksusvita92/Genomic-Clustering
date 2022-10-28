4b19eed1ec4e1e662c2b8a7e019bfe3476305c17
Kurnia Susvitasari <ksusvita@sfu.ca>
2022-10-28 11:26:29 -0700

    Plot Figure 7 (expected cluster size)

--- analysis scripts/plot cluster size.R
+++ analysis scripts/plot cluster size.R
@@ -9,6 +9,7 @@
 
 library(ggplot2)
 library(dplyr)
+library(patchwork)
 
 dt <- read.csv("./results/exp_cluster_size.csv")
 dt$method <- factor(dt$method, levels = c("plr", "mlr"))
@@ -17,7 +18,7 @@ dt$method <- factor(dt$method, levels = c("plr", "mlr"))
 ## ******* Create boxplot *******
 ## ******************************
 # create expected cluster size without unclustered cases
-dt %>% 
+pl1 <- dt %>% 
   filter(rho == 0) %>%
   ggplot() +
   geom_violin(aes(x = method, y = size, fill = method), alpha = .4) +
@@ -27,12 +28,14 @@ dt %>%
   labs(x = "", y = "size")
 
 # create expected cluster size with unclustered cases
-dt %>% 
-  filter(rho != 0) %>%
+pl2 <- dt %>% 
+  filter(rho > 0) %>%
   ggplot() +
   geom_violin(aes(x = method, y = size, fill = method), alpha = .4) +
   geom_hline(aes(yintercept = tr_size), lty = "dashed") +
   facet_wrap(~cluster, nrow = 4) +
-  theme_light() +
+  theme_light() + theme(legend.position = "none", legend.title = element_blank()) +
   labs(x = "", y = "size")
+
+pl1/pl2
 #
